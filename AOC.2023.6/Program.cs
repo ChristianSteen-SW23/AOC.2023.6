477a486ad8c990625cc492cbd915e8056b452d77
ChristianSteen-SW23 <cniel@student.aau.dk>
2023-12-13 12:28:23 +0100

    Add project files.

---
+++ AOC.2023.6/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AOC._2023._6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
