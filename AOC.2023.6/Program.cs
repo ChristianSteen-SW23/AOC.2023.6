9629dc66300a018fa570f8fdb3fa8c39f295e364
ChristianSteen-SW23 <cniel@student.aau.dk>
2023-12-13 12:30:57 +0100

    Lavet lidt

--- AOC.2023.6/Program.cs
+++ AOC.2023.6/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            int[] timeRecord = { 38, 67, 76, 73 };
+            int[] distanceRecord = { 234,1027,1157,1236};
+
+            for (int i = 0; i < timeRecord.Length; i++)
+            {
+                for (int j = 0; j < distanceRecord.Length; j++) { 
+                
+                }
+            }
             Console.WriteLine("Hello, World!");
         }
     }
